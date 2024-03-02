89c305356d062f46af3a25c266e1808d628290dc
DaveDarko23 <a22110098@ceti.mx>
2024-03-01 21:38:37 -0600

    Autenticación de usuarios v1.2

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { User } from "../types/user.type"
 import UserService from "../services/user.service"
+import passport from "passport"
 
 const router = express.Router()
 const service = new UserService()
@@ -15,15 +16,19 @@ router.post("/", async (req, res, next) => {
   }
 })
 
-router.get("/", async (req, res, next) => {
-  try {
-    const { email } = req.query
-    const user = await service.findByEmail(email as string)
-    console.log({ user })
-    res.status(200).json({ user })
-  } catch (error) {
-    next(error)
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      const { email } = req.query
+      const user = await service.findByEmail(email as string)
+      console.log({ user })
+      res.status(200).json({ user })
+    } catch (error) {
+      next(error)
+    }
   }
-})
+)
 
 export default router
